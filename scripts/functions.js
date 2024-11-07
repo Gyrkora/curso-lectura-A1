7a9987a78aac4a87fd71c65ef1429b2e4164246f
Gyrkora <giselle.gallardo0210@gmail.com>
2024-11-06 21:39:24 -0300

    boton styles

---
+++ scripts/functions.js
@@ -0,0 +1,7 @@
+function openSmallWindow() {
+    window.open(
+      'https://www.aulatow.com/mod/h5pactivity/view.php?id=684', 
+      'ventanaPequena', 
+      'width=400,height=500, right=50,top=50' 
+    );
+  }
